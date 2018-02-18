3b1e2a1d6f3400e86c51151283b91283c02c70a3
Oleh Horitsyn <oleggoritsyn@cloud.upwork.com>
2018-02-18 20:06:47 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-
+import { HistoryModule } from './history/history.module';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule,
+    HistoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
